e19ab5378acb8cc6bc06ebf099d98c97267537a0
hongkha336 <hongkha336@gmail.com>
2019-11-21 07:30:33 +0700

    Update contne-edit.directive

--- src/app/directives/common/content-edit.directive.spec.ts
+++ src/app/directives/common/content-edit.directive.spec.ts
@@ -1,8 +1,1 @@
-import { ContentEditDirective } from './content-edit.directive';
 
-describe('ContentEditDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ContentEditDirective();
-    expect(directive).toBeTruthy();
-  });
-});
